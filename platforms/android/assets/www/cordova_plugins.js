ad15a8d9c69fcd1f32c78ac23dca18986afa5507
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2018-01-22 05:45:55 +0000

    local notifications

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,45 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-badge.Badge",
-    "file": "plugins/cordova-plugin-badge/www/badge.js",
-    "pluginId": "cordova-plugin-badge",
-    "clobbers": [
-      "cordova.plugins.notification.badge"
-    ]
-  },
-  {
     "id": "cordova-plugin-device.device",
     "file": "plugins/cordova-plugin-device/www/device.js",
     "pluginId": "cordova-plugin-device",
     "clobbers": [
       "device"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Core",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Util",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "merges": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
     ]
   },
   {
@@ -83,20 +49,47 @@ module.exports = [
       "cordova.plugins.Keyboard"
     ],
     "runs": true
+  },
+  {
+    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+    "pluginId": "de.appplant.cordova.plugin.local-notification",
+    "clobbers": [
+      "cordova.plugins.notification.local",
+      "plugin.notification.local"
+    ]
+  },
+  {
+    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
+    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
+    "pluginId": "de.appplant.cordova.plugin.local-notification",
+    "clobbers": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
+    ]
+  },
+  {
+    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
+    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
+    "pluginId": "de.appplant.cordova.plugin.local-notification",
+    "merges": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-badge": "0.8.6",
   "cordova-plugin-device": "1.1.4",
   "cordova-plugin-ionic-webview": "1.1.16",
-  "cordova-plugin-local-notification": "0.9.0-beta.2",
   "cordova-plugin-network-information": "2.0.1",
   "cordova-plugin-networkinterface": "1.2.0",
   "cordova-plugin-splashscreen": "4.0.3",
   "cordova-plugin-whitelist": "1.3.1",
-  "ionic-plugin-keyboard": "2.2.1"
+  "ionic-plugin-keyboard": "2.2.1",
+  "cordova-plugin-app-event": "1.2.1",
+  "de.appplant.cordova.plugin.local-notification": "0.8.5"
 };
 // BOTTOM OF METADATA
 });
