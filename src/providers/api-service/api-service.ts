208862dcf5cd571b13911bc5a6425edbe58fb9a2
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2018-04-01 05:44:45 +0000

    iframe

--- src/providers/api-service/api-service.ts
+++ src/providers/api-service/api-service.ts
@@ -35,28 +35,13 @@ export class APIServiceProvider {
     headers.append('Accept-Language', 'en-US,en;q=0.9,es-PA;q=0.8,es;q=0.7');
     headers.append('Upgrade-Insecure-Requests', '1');
     return new Promise((resolve, reject) => {
-      /*this.http.get(Constants.myMatApiIndexUrl, { headers: headers })
+      this.http.get(Constants.myMatApiIndexUrl, { headers: headers })
       .map(res => res.text())
       .subscribe(res => {
         resolve(res);
       }, (err) => {
         reject(err);
-      });*/
-      $.ajax({ url: Constants.myMatApiIndexUrl, timeout: 5000 })
-        .done(function(response){ 
-          var values = '';
-          for(var key in response) {
-            values += key + ',' + response[key] + ';';
-          }
-          alert('good: ' + values); 
-          resolve(response); })
-        .fail(function(response){
-          var values = '';
-          for(var key in response) {
-            values += key + ',' + response[key] + ';';
-          }
-          alert('error: ' + values); 
-          reject(response); });
+      });
     });
   }
   
