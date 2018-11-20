39d37bdb8278e8b09006d1b8b1d13a285592871d
Luis Ricardo Sanchez <lsanchez@esferasoluciones.com>
2018-11-20 00:21:07 -0500

    cambios finales

--- platforms/android/app/build/generated/source/buildConfig/release/com/esferasoluciones/mymat_light/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/esferasoluciones/mymat_light/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.esferasoluciones.mymat_light";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 80305;
-  public static final String VERSION_NAME = "8.3.5";
+  public static final int VERSION_CODE = 80306;
+  public static final String VERSION_NAME = "8.3.6";
 }
