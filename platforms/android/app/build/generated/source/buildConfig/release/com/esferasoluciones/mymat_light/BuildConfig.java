61653cabb7cf0551b7ff989fd472a0771282bdf2
Luis Ricardo Sanchez <lsanchez@esferasoluciones.com>
2019-05-27 14:57:43 -0500

    version android 8.4.14

--- platforms/android/app/build/generated/source/buildConfig/release/com/esferasoluciones/mymat_light/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/esferasoluciones/mymat_light/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.esferasoluciones.mymat_light";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 80412;
-  public static final String VERSION_NAME = "8.4.12";
+  public static final int VERSION_CODE = 80414;
+  public static final String VERSION_NAME = "8.4.14";
 }
